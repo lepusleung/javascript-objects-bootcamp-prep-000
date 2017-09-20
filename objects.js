0a447d1c63e80af9cb67200064d58b300ef9ec53
Man Leung <lepusleung@gmail.com>
2017-09-20 14:21:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,11 +7,11 @@ var playlist = {
 function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle;
   return playlist;
-  
+
 }
 
 function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName];
   return playlist;
-  
+
 }
